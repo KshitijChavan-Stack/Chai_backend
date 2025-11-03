cb4ca6f04f6fdd74ee72a6c99120a63e1f431f8a
KshitijChavan-Stack <chavankshitij0@gmail.com>
2025-11-03 15:35:43 +0530

    add subscription schema comments and include subscription schema image

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -1,7 +1,14 @@
 import { model, Schema } from "mongoose";
 
+// every time we subscribe to a channel or (vice versa) a new
+// document is created !!
 const subscription = new Schema(
   {
+    // when we have to find how many subs
+    // a channel have we'll count how many documents
+    // have CHANNEL sub to -> chai aur code
+    // when we want to find subs of a perticular channel
+    // we have to find by channel
     subscriber: {
       type: Schema.Types.ObjectId,
       // one who's subscribing
